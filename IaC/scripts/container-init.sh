a81c229140e868b8506d2076814ff6d301c8e92d
Jamal Shahverdiyev <jamal.shahverdiev@gmail.com>
2021-01-09 17:46:52 +0300

    Updated Provision script

--- IaC/scripts/container-init.sh
+++ IaC/scripts/container-init.sh
@@ -11,11 +11,11 @@ apt update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 wget https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-Linux-x86_64 -O /bin/docker-compose && chmod +x /bin/docker-compose
 
-if [[ -z "$MY_PUBLIC_IP" || -z "$API_TOKEN" ]]
+if [ -z "$MY_PUBLIC_IP" -o -z "$API_TOKEN" ]
 then
       echo "\$MY_PUBLIC_IP or \$API_TOKEN variable is empty" >> ~/variables-status.txt
 else
-      echo "\$MY_PUBLIC_IP or \$API_TOKEN variable is NOT empty" >> ~/variables-status.txt
+      echo "\$MY_PUBLIC_IP and \$API_TOKEN variables are NOT empty" >> ~/variables-status.txt
 fi
 
 git clone --branch cron-in-docker 'https://github.com/jamalshahverdiev/gists-activity-api.git' $cloneFolder && sed -i "s/replace_api_token/${API_TOKEN}/g" $cloneFolder/check_users_activity.py && cd $cloneFolder && docker-compose up -d
